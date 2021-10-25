3549fa64db91a0745dce1e0a463ec801b8ec69c7
sonali_sohoni <ssohoni@camsbycbs.com>
2021-10-25 15:19:55 -0400

    PORT change

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = env.process.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require("./data/animals.json");
 
