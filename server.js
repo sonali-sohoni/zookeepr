62d379739632c1e6d652007b9d2be2f8bbecb2e7
sonali_sohoni <ssohoni@camsbycbs.com>
2021-10-26 15:33:39 -0400

    API functionality complete

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const { animals } = require("./data/animals.json");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require("./data/animals.json");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
 	let filteredResults = animalsArray;
@@ -23,10 +28,12 @@ function filterByQuery(query, animalsArray) {
 	}
 
 	if (query.diet) {
-		filteredResults = animalsArray.filter((value) => value.diet === query.diet);
+		filteredResults = filteredResults.filter(
+			(value) => value.diet === query.diet
+		);
 	}
 	if (query.species) {
-		filteredResults = animalsArray.filter(
+		filteredResults = filteredResults.filter(
 			(animal) => animal.species === query.species
 		);
 	}
@@ -42,6 +49,18 @@ const findById = (id, animals) => {
 	return animals.filter((animal) => animal.id === id)[0];
 };
 
+const createNewAnimal = (body, animalsArr) => {
+	console.log(body);
+	const animal = body;
+	animalsArr.push(animal);
+	console.log(__dirname);
+	fs.writeFileSync(
+		path.join(__dirname, "./data/animals.json"),
+		JSON.stringify({ animals: animalsArr }, null, 2)
+	);
+	return animal;
+};
+
 app.get("/api/animals", (req, res) => {
 	let results = animals;
 	if (req.query) {
@@ -51,12 +70,39 @@ app.get("/api/animals", (req, res) => {
 	res.json(results);
 });
 
+function validateAnimal(animal) {
+	if (!animal.name || typeof animal.name !== "string") {
+		return false;
+	}
+	if (!animal.species || typeof animal.species !== "string") {
+		return false;
+	}
+	if (!animal.diet || typeof animal.diet !== "string") {
+		return false;
+	}
+	if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+		return false;
+	}
+	return true;
+}
+
 app.get("/api/animals/:id", (req, res) => {
 	const results = findById(req.params.id, animals);
 	if (results) {
 		res.json(results);
 	} else {
 		res.send(404);
+	}
+});
+
+app.post("/api/animals", (req, res) => {
+	console.log(req.body);
+	req.body.id = animals.length.toString();
+	if (!validateAnimal(req.body)) {
+		res.status(400).send("The animal is not properly formatted.");
+	} else {
+		const animal = createNewAnimal(req.body, animals);
+		res.json(animal);
 	}
 });
 
