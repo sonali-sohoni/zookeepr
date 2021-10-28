dcdf3446926daefa76b8d2b269cfe62996beedb3
sonali_sohoni <ssohoni@camsbycbs.com>
2021-10-28 18:11:50 -0400

    front end integration

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
 	let filteredResults = animalsArray;
@@ -104,6 +105,24 @@ app.post("/api/animals", (req, res) => {
 		const animal = createNewAnimal(req.body, animals);
 		res.json(animal);
 	}
+});
+
+//FI 1. client requests index.html
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
